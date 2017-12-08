445e50b5472d61261ff18468295f6cecdde11783
Jim Whurr <jim.whurr@outlook.com>
2017-12-08 13:41:28 +0000

    Added dis/connect handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const socketIO = require('socket.io');
 const path = require('path');
+const http = require('http');
 
+const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const publicPath = path.join(__dirname, '../public');
+// get express to use our http server
+const server = http.createServer(app);
+
+// change server to use socket.io
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(PORT, () => {
     console.log(`Server available on port ${PORT}. Press ^C to exit.`);
 });
